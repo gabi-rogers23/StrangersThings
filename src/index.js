5deab2bd68c047222f99bb571956eb20ebd931cd
Gabrielle Rogers <gabirogers23@gmail.com>
2022-12-12 20:01:40 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>
+    <h1>Stranger's Things!</h1>
+  </div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
