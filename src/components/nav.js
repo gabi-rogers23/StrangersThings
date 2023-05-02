876766023a2df108b3154aae57a21d01d173658b
gabi-rogers23 <gabirogers23@gmail.com>
2023-05-02 13:29:16 -0500

    hamburger menu showing up when screen is less than 600px

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const App = (props) => {
   return (
@@ -10,37 +11,42 @@ const App = (props) => {
         <div className="tagline"> TRASH TO TREASURE</div>
       </div>
       <nav className="topNav">
-        <NavLink exact to="/" activeClassName="topNavActive">
-          HOME
-        </NavLink>
-
-        {localStorage.getItem("auth_token") ? (
-          <>
-            <NavLink
-              to="/Login"
-              activeClassName="topNavActive"
-              onClick={() => {
-                console.log("Logged In " + props.isLoggedIn);
-                props.setIsLoggedIn(false);
-                localStorage.clear();
-                console.log("Log Out Click " + props.isLoggedIn);
-              }}
-            >
-              LOG OUT
-            </NavLink>
-
-            <NavLink to="/profile" activeClassName="topNavActive">
-              PROFILE
-            </NavLink>
-          </>
-        ) : (
-          <NavLink to="/Login" activeClassName="topNavActive">
-            LOG IN
+        <div className="navIcon">
+          <GiHamburgerMenu />
+        </div>
+        <div className="navLinks">
+          <NavLink exact to="/" activeClassName="topNavActive">
+            HOME
           </NavLink>
-        )}
-        <NavLink to="/Posts" activeClassName="topNavActive">
-          POSTS
-        </NavLink>
+
+          {localStorage.getItem("auth_token") ? (
+            <>
+              <NavLink
+                to="/Login"
+                activeClassName="topNavActive"
+                onClick={() => {
+                  console.log("Logged In " + props.isLoggedIn);
+                  props.setIsLoggedIn(false);
+                  localStorage.clear();
+                  console.log("Log Out Click " + props.isLoggedIn);
+                }}
+              >
+                LOG OUT
+              </NavLink>
+
+              <NavLink to="/profile" activeClassName="topNavActive">
+                PROFILE
+              </NavLink>
+            </>
+          ) : (
+            <NavLink to="/Login" activeClassName="topNavActive">
+              LOG IN
+            </NavLink>
+          )}
+          <NavLink to="/Posts" activeClassName="topNavActive">
+            POSTS
+          </NavLink>
+        </div>
       </nav>
     </header>
   );
